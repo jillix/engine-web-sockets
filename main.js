b245880b30d6fc66d9dd58026867dbf6a1bf9819
Ionică Bizău <bizauionica@yahoo.com>
2015-02-11 11:10:51 +0200

    Open the link

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-exports.init = function () {};
+exports.init = function () {
+    this.ws = this.link("_clientConnected").send().data(function (err, data) {
+        this.emit("data", err, data);
+    }.bind(this));
+};
